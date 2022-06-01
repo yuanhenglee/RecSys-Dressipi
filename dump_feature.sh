abb5f264d18d117a3fdba761c92e2407d64dbd4b
yuanhenglee <1990henrylee@gmail.com>
2022-06-01 22:30:47 +0800

    add: test features in dump feature script

--- dump_feature.sh
+++ dump_feature.sh
@@ -2,3 +2,4 @@ python3 ./src/VSM/dump_purchase.py
 python3 ./src/VSM/dump_session.py
 python3 ./src/VSM/dump_embedding.py
 python3 src/VSM/feature_generate.py --with_purchase
+python3 src/VSM/feature_generate.py --session_path dataset/test_sessions.pickle --output_path dataset/test_features/session.csv
