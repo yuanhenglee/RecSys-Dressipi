26dd76190542a7e4fd1df135521cb834c9cfba0f
yuanhenglee <1990henrylee@gmail.com>
2022-06-01 21:32:04 +0800

    add: add feature generate into dump script

---
+++ dump_feature.sh
@@ -0,0 +1,4 @@
+python3 ./src/VSM/dump_purchase.py
+python3 ./src/VSM/dump_session.py
+python3 ./src/VSM/dump_embedding.py
+python3 src/VSM/feature_generate.py --with_purchase
--- dump_local_pickle.sh
+++
@@ -1,3 +0,0 @@
-python3 ./src/VSM/dump_purchase.py
-python3 ./src/VSM/dump_session.py
-python3 ./src/VSM/dump_embedding.py
