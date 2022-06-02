f2dea5d64b8a605d9d717700cf5a3e7d7816a3cf
yuanhenglee <1990henrylee@gmail.com>
2022-06-02 12:26:26 +0800

    fix: add mkdir before dump feature

--- dump_feature.sh
+++ dump_feature.sh
@@ -1,3 +1,7 @@
+# make sure dataset/train_features and dataset/test_features exist
+mkdir -p dataset/train_features
+mkdir -p dataset/test_features
+
 python3 ./src/VSM/dump_purchase.py
 python3 ./src/VSM/dump_session.py
 python3 src/VSM/dump_session.py --session_path dataset/test_leaderboard_sessions.csv --output_path dataset/test_sessions.pickle
