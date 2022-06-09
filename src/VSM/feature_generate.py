f25a6429b58e6e3c291cbb45d834fb2bdef9c04c
yuanhenglee <1990henrylee@gmail.com>
2022-06-09 17:28:10 +0800

    fix: feature_generate features based on top item

--- src/VSM/feature_generate.py
+++ src/VSM/feature_generate.py
@@ -39,35 +39,50 @@ feature_cols = [
 def cosine_similarity(v1, v2, inner):
     return inner/(np.linalg.norm(v1)*np.linalg.norm(v2))
 
-
-def euclidean_similarity(v1, v2):
-    return np.linalg.norm(v1-v2)
-
-
 def inner_similarity(v1, v2):
     return np.dot(v1, v2)
 
-
-def sec2July(date1):
-    d1 = datetime.strptime(date1[:19], "%Y-%m-%d %H:%M:%S")
-    d2 = datetime.strptime("2021-07-31 12:00:00", "%Y-%m-%d %H:%M:%S")
-    return (d2 - d1).total_seconds()
-
-
-def build_features( item_vectors ):
-    # for each candidate_item
-    features_list = np.zeros((N, len(feature_cols)))
-
-    # item dic for recording item_id, inner product
-    item_dic = {}  # //
+def select_top_inner( item_vector, purchase_id ):
+    # cal all N inner 
+    N_inner = np.zeros(N)
     for i in range(N):
         candidate_vector = vector_space[candidate_items[i]]
+        N_inner[i] = inner_similarity(item_vector, candidate_vector)
+    
+    selected_ids = [ candidate_items[i] for i in np.argsort(N_inner)[-1*n_train_sample:]]
+
+    if purchase_id in selected_ids or purchase_id not in candidate_items:
+        return selected_ids
+    else:
+        return [purchase_id] + selected_ids[1:]
+
+
+def build_features( item_vectors, session_id ):
+    y = None 
+    if with_purchase:
+        purchase_id, _ = purchase_dict[session_id]
+        selected_ids = select_top_inner( item_vectors[0], purchase_id )
+        y = np.zeros((n_train_sample, 1))
+    else:
+        selected_ids = candidate_items
+    # for each candidate_item
+    features_list = np.zeros((n_train_sample, len(feature_cols)))
+
+    # item dic for recording item_id, inner product
+    # item_dic = {}  # //
+    for i in range(n_train_sample):
+        candidate_item = selected_ids[i]
+        candidate_vector = vector_space[candidate_item]
 
         top_items_inner = [inner_similarity(item_vector, candidate_vector) for item_vector in item_vectors]
-        top_items_cosine= [cosine_similarity(item_vectors[i], candidate_vector, top_items_inner[i]) for i in range(len(item_vectors))]
+        # top_items_cosine= [cosine_similarity(item_vectors[i], candidate_vector, top_items_inner[i]) for i in range(len(item_vectors))]
 
         # item_id label, for easier sampling later
-        features_list[i][0] = candidate_items[i]
+        features_list[i][0] = candidate_item
+
+        if with_purchase and candidate_item == purchase_id:
+            y[i][0] = 1
+
         features_list[i][1] = top_items_inner[0]
         features_list[i][2] = np.mean(top_items_inner)
         features_list[i][3] = np.max(top_items_inner)
@@ -75,26 +90,21 @@ def build_features( item_vectors ):
         # features_list[i][5] = np.mean(top_items_cosine)
         # features_list[i][6] = np.max(top_items_cosine)
 
-        item_dic[i] = (top_items_inner[0])  # //
+        # item_dic[i] = (top_items_inner[0])  # //
 
-    sort_dic = {k: v for k, v in sorted(
-        item_dic.items(), key=lambda item: item[1], reverse=True)}  # //
+    # sort_dic = {k: v for k, v in sorted(
+    #     item_dic.items(), key=lambda item: item[1], reverse=True)}  # //
 
-    return features_list, sort_dic  # //
+    return features_list, y
 
 
-def combine_items_features(item_list):
+def combine_items_features( session_id ):
+    item_list = session_dict[session_id]
 
     top_item_ids = [item_id for item_id, _ in item_list[:3]]
     top_item_vectors = [vector_space[item_id] for item_id in top_item_ids]
-    # vector_list = [vector_space[item_id] for item_id, date in item_list]
-    # sec_list = [sec2July(date) for item_id, date in item_list]
 
-    # simply sum up the vectors
-    # combined_vector = np.mean(vector_list, axis=0)
-
-    # combined_sec = round(np.mean(sec_list))
-    return build_features( top_item_vectors )
+    return build_features( top_item_vectors, session_id )
 
 
 # args
@@ -150,6 +160,7 @@ except:
 
 start = 0
 save_period = 10000
+# end = 5
 end = len(session_dict)
 # construct df
 
@@ -160,44 +171,45 @@ features_lists = []
 y_lists = []
 for i in tqdm(range(start, end)):
     session_id = list(session_dict.keys())[i]
-    item_list = session_dict[session_id]
-    features_list, item_dic = combine_items_features(item_list)  # //
+    X, y = combine_items_features(session_id)  # //
 
     # training data with purchase
     if with_purchase:
-        purchase_id, purchase_date = purchase_dict[session_id]
-        y = [ 1 if purchase_id == candidate_item else 0 for candidate_item in candidate_items ]
+    #     purchase_id, purchase_date = purchase_dict[session_id]
+    #     y = [ 1 if purchase_id == candidate_item else 0 for candidate_item in candidate_items ]
 
-        # only keep first 100 inner product value
-        sort_list = []
-        y_sort_list = []
-        num = 0
-        flag = False
-        for k in item_dic.keys():
-            if num >= n_train_sample and flag == True:
-                break
-            num += 1
+    #     # only keep first 100 inner product value
+    #     sort_list = []
+    #     y_sort_list = []
+    #     num = 0
+    #     flag = False
+    #     for k in item_dic.keys():
+    #         if num >= n_train_sample and flag == True:
+    #             break
+    #         num += 1
 
-            if num >= n_train_sample and flag == False:
-                # if features_list[k][3] == 1:
-                if y[k] == 1:
-                    sort_list.append(features_list[k])
-                    y_sort_list.append([y[k]])
-                    break
-                else:
-                    continue
+    #         if num >= n_train_sample and flag == False:
+    #             # if features_list[k][3] == 1:
+    #             if y[k] == 1:
+    #                 sort_list.append(features_list[k])
+    #                 y_sort_list.append([y[k]])
+    #                 break
+    #             else:
+    #                 continue
 
-            sort_list.append(features_list[k])
-            y_sort_list.append([y[k]])
+    #         sort_list.append(features_list[k])
+    #         y_sort_list.append([y[k]])
 
-            if y[k] == 1:
-            # if features_list[k][3] == 1:
-                flag = True
+    #         if y[k] == 1:
+    #         # if features_list[k][3] == 1:
+    #             flag = True
 
-        features_lists.extend(sort_list)
-        y_lists.extend(y_sort_list)
+    #     features_lists.extend(sort_list)
+    #     y_lists.extend(y_sort_list)
 
-    # //
+    # # //
+        features_lists.extend(X)
+        y_lists.extend(y)
 
         if i % save_period == save_period-1 or i == end-1:
             if as_pickle:
@@ -225,11 +237,11 @@ for i in tqdm(range(start, end)):
     else:
         if as_pickle:
             with open( output_path + '_' + 'X' + '_' + str(session_id) + '.pickle', 'wb' ) as f:
-                pickle.dump( features_list, f , pickle_protocol)
+                pickle.dump( X, f , pickle_protocol)
         else:
             with open(output_path + '_' + 'X' + '_' + str(session_id) + '.csv', 'w') as f:
                 wr = csv.writer(f)
                 wr.writerow( feature_cols )
-                wr.writerows(features_list)
+                wr.writerows(X)
 
 print("Done. Execution Time:", time.time() - start_time)
